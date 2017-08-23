3602057f0a5a5169b88fc984c6a827c3d68d1462
Alim Charaniya <alimcharaniya25@gmail.com>
2017-08-22 18:15:59 -0700

    working on sending timeout when pi loses connection

--- canbus-node/main.js
+++ canbus-node/main.js
@@ -1,23 +1,46 @@
 var can = require('rawcan');
 var telemetry = require('./telemetry.js');
+var canSocket = can.createSocket('can0');
+var serverAddress = ('http://192.168.0.13:8080');
+var ws = require('socket.io-client');
+var io = ws.connect(serverAddress);
+var Stopwatch = require('timer-stopwatch');
 
-var socket = can.createSocket('can0');
+var timer = new Stopwatch(3000); //start a 3 stop watch 
+
+timer.onTime(function(time){
+   console.log(time.ms);
+})
+
+timer.onDone(function(){
+   console.log("NETWORK TIMEOUT - ACTUATE BRAKES");
+})
+
+io.on('connect',function(data){
+   console.log("Client connected to server. Verify on server machine");
+   timer.start(); 
+   io.on("ping",function(){
+      timer.stop();
+      timer.reset(3000) //when pinged, reset timer
+      timer.start();
+      console.log("pinged!");
+      io.emit("pong-back");
+   })
+})
 
 
-function sendEmergencyStop(){
-socket.send(0x121, 'STOP');
-}
 
 
+//If can bus socket throws error, print the error
+canSocket.on('error', err => {console.log('socket error: ' + err);});
 
-socket.on('error', err => {console.log('socket error: ' + err);});
-
-
-socket.on('message', (id, buffer) => {
+//print all frames in raw id + buffer format 
+canSocket.on('message', (id, buffer) => {
 	console.log('got frame: ' + id.toString(16) +"buffer: " + buffer.toString('hex'));
-	socket.send(86, 'hello');
+	//canSocket.send(86, 'hello');
 	console.log("after message");
 		 
 });
 
+console.log("program compiled..listening..");
 
